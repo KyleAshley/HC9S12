f1b89e42dbcee3efbf36eced9fa9f63c6f6cfa0b
Kyle Ashley <kyle.ashley183@gmail.com>
2014-01-12 09:52:08 -0500

    Create 7seg.c

---
+++ 7seg.c
@@ -0,0 +1,171 @@
+/******************************************************************************/
+// 7SEG FUNCTIONS
+// Author: Kyle Ashley
+// To be used with MC9S12 uP
+/******************************************************************************/
+#include "7seg.h"
+#include <hidef.h>          /* common defines and macros */
+#include "derivative.h"      /* derivative-specific definitions */
+/******************************************************************************/
+// PRE: NONE
+// POST: seven-segment display is configured to be written to
+// (Common Anode)
+/******************************************************************************/
+void sev_setup(void)
+{
+    DDRH = 0xFF;        // Configure PTH for Output
+}
+
+/******************************************************************************/
+// PRE: NONE
+// POST: Character is written to the seven-segment display (if applicable)
+// (Common Anode)
+/******************************************************************************/
+void sev_write(char symbol)
+{
+    // NUMERIC?
+    if(symbol >= 48 && symbol <= 57)
+        switch (symbol)
+        {
+            case '0':
+                PTH = 0x40;
+                break;
+            case '1':
+                PTH = 0x79;
+                break;
+            case '2':
+                PTH = 0x24;
+                break;
+            case '3':
+                PTH = 0x30;
+                break;
+            case '4':
+                PTH = 0x19;
+                break;
+            case '5':
+                PTH = 0x12;
+                break;
+            case '6':
+                PTH = 0x02;
+                break;
+            case '7':
+                PTH = 0x78;
+                break;
+            case '8':
+                PTH = 0x00;
+                break;
+            case '9':
+                PTH = 0x10;
+                break;
+        }
+        // ALPHABETICAL
+        else
+        {
+            if(symbol == 'a' || symbol == 'A')
+                PTH = 0x08;
+            else if(symbol == 'b' || symbol == 'B')
+                PTH = 0x03;
+            else if(symbol == 'c')
+                PTH = 0x27;
+            else if(symbol == 'C')
+                PTH = 0x46;
+            else if(symbol == 'd' || symbol == 'D')
+                PTH = 0x21;
+            else if(symbol == 'e' || symbol == 'E')
+                PTH = 0x06;
+            else if(symbol == 'f' || symbol == 'F')
+                PTH = 0x0E;
+            else if(symbol == 'h')
+                PTH = 0x0B;
+            else if(symbol == 'H')
+                PTH = 0x09;
+            else if(symbol == 'l')
+                PTH = 0x4F;
+            else if(symbol == 'L')
+                PTH = 0x47;
+            else if(symbol == 'n' || symbol == 'N')
+                PTH = 0x2B;
+            else if(symbol == 'o' || symbol == 'O')
+                PTH = 0x23;
+            else if(symbol == 'p' || symbol == 'P')
+                PTH = 0x0E;
+            else if(symbol == 'q' || symbol == 'Q')
+                PTH = 0x18;
+            else if(symbol == 'r' || symbol == 'R')
+                PTH = 0x2F;
+            else if(symbol == 'u')
+                PTH = 0x63;
+            else if(symbol == 'U')
+                PTH = 0x41;
+            else if(symbol == 'y' || symbol == 'Y')
+                PTH = 0x11; 
+            
+            // Default NOT A LETTER
+            else
+            PTH = 0x3F;
+        }
+        LCDDelaySCROLL(1);
+}
+
+/******************************************************************************/
+// PRE: NONE
+// POST: Tests Segments by displaying all characters
+// (Common Anode)
+/******************************************************************************/
+void sev_test() 
+{
+   sev_setup();
+   sev_write('0');
+   LCDDelaySCROLL(5);
+   sev_write('1');
+   LCDDelaySCROLL(5);
+   sev_write('2');
+   LCDDelaySCROLL(5);
+   sev_write('3');
+   LCDDelaySCROLL(5);
+   sev_write('4');
+   LCDDelaySCROLL(5);
+   sev_write('5');
+   LCDDelaySCROLL(5);
+   sev_write('6');
+   LCDDelaySCROLL(5);
+   sev_write('7');
+   LCDDelaySCROLL(5);
+   sev_write('8');
+   LCDDelaySCROLL(5);
+   sev_write('9');
+   LCDDelaySCROLL(5);
+   
+   sev_write('a');
+   LCDDelaySCROLL(5);
+   sev_write('b');
+   LCDDelaySCROLL(5);
+   sev_write('c');
+   LCDDelaySCROLL(5);
+   sev_write('d');
+   LCDDelaySCROLL(5);
+   sev_write('e');
+   LCDDelaySCROLL(5);
+   sev_write('f');
+   LCDDelaySCROLL(5);
+   sev_write('h');
+   LCDDelaySCROLL(5);
+   sev_write('L');
+   LCDDelaySCROLL(5);
+   sev_write('n');
+   LCDDelaySCROLL(5);
+   sev_write('o');
+   LCDDelaySCROLL(5);
+   sev_write('p');
+   LCDDelaySCROLL(5);
+   sev_write('q');
+   LCDDelaySCROLL(5);
+   sev_write('r');
+   LCDDelaySCROLL(5);
+   sev_write('u');
+   LCDDelaySCROLL(5);
+   sev_write('y');
+   LCDDelaySCROLL(5);
+}
+   
+
